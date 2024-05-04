ef2e7aedc185c09ba0ee05ef23df8776035b9400
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-05-04 10:15:45 +0500

    I learn data type in Javascript , alert , prompt and confirm function.

---
+++ index.js
@@ -0,0 +1,29 @@
+// String Data Type
+
+let message = "Hello World";
+
+// Use of Single back tick
+console.log(`${message} in the Javascript Programming`);
+
+// Number Data Type
+let number = 45;
+// Use of NaN
+let result = 45/NaN;
+console.log(result);
+
+// Big int Data Type for (2^53-1)
+const n = 1234567890123456789012345678901234567890n;
+console.log(typeof(n));
+
+// Boolean Data Type
+let isGreater = 4 > 1;
+
+console.log( isGreater ); 
+
+// Null Data type
+let age = null;
+console.log(typeof(age));
+
+// Undefined Data type
+let age1;
+console.log(typeof(age1));
